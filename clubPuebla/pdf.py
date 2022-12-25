03b61c80ed62fa2f4f54c40d60456d50c71c1dbb
alnsuarez <alansuarezsan@gmail.com>
2022-12-25 16:38:48 -0600

    pdfv3

--- clubPuebla/pdf.py
+++ clubPuebla/pdf.py
@@ -5,14 +5,15 @@ from django.template.loader import get_template
 from django.views import View
 import pdfkit
 
-def render_to_pdf(template_src, context_dict={}, filename=""):
+def render_to_pdf(template_src, context_dict={}, name=""):
 	template = get_template(template_src)
 	html  = template.render(context_dict)
 	f = open('temp.html', 'w')
-	# options = {
-    #       'page-size': 'Letter',
-    #       'encoding': "UTF-8",
-    #    }
-	pdf = pdfkit.from_string(html, filename)
+	options = {
+          'page-size': 'Letter',
+          'encoding': "UTF-8",
+       }
+	pdf = pdfkit.from_string(html, options=options)
 	response = HttpResponse(pdf,content_type='application/pdf')
+	response['Content-Disposition'] = 'attachment; filename=%s.pdf'% name
 	return response
