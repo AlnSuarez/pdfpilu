00f9022e48c976b87cdb6a26d148fde0ac70460a
alnsuarez <alansuarezsan@gmail.com>
2022-12-25 16:41:17 -0600

    pdf v3.2

--- clubPuebla/pdf.py
+++ clubPuebla/pdf.py
@@ -5,7 +5,7 @@ from django.template.loader import get_template
 from django.views import View
 import pdfkit
 
-def render_to_pdf(template_src, context_dict={}, name=""):
+def render_to_pdf(template_src, context_dict={}):
 	template = get_template(template_src)
 	html  = template.render(context_dict)
 	f = open('temp.html', 'w')
@@ -13,6 +13,6 @@ def render_to_pdf(template_src, context_dict={}, name=""):
           'page-size': 'Letter',
           'encoding': "UTF-8",
        }
-	pdf = pdfkit.from_string(html, options=options)
+	pdf = pdfkit.from_string(html, False, options=options)
 	response = HttpResponse(pdf,content_type='application/pdf')
 	return response
--- clubPuebla/views.py
+++ clubPuebla/views.py
@@ -10,6 +10,6 @@ from django.views.decorators.csrf import csrf_exempt
 @csrf_exempt
 @require_http_methods(["POST"])
 def templateRed(request):
-    pdf=render_to_pdf("templateRed.html", json.loads(request.body), "plantillaPuebla.pdf")
+    pdf=render_to_pdf("templateRed.html", json.loads(request.body))
     return HttpResponse(pdf, content_type="application/pdf")
 
